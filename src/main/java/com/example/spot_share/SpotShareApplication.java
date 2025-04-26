e2ee926dc747835f36c422984cad7d9dcc7306ce
Nitish <mail.logicninjax@gmail.com>
2025-04-26 10:39:19 +0530

    initial commit

---
+++ src/main/java/com/example/spot_share/SpotShareApplication.java
@@ -0,0 +1,13 @@
+package com.example.spot_share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spot_share/SpotShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spot_share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
