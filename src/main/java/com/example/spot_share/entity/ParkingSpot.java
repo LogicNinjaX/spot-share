2527142c25626ca15d10b8d38d61f8ac4c75438c
Nitish <mail.logicninjax@gmail.com>
2025-04-27 08:53:29 +0530

    add parking entity

---
+++ src/main/java/com/example/spot_share/entity/ParkingSpot.java
@@ -0,0 +1,117 @@
+package com.example.spot_share.entity;
+
+import com.example.spot_share.enums.ParkingStatus;
+import com.example.spot_share.enums.VehicleType;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+public class ParkingSpot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID parkingId;
+
+    @ManyToOne
+    private User owner; // owner data
+
+    private String location;
+
+    private double pricePerHour;
+
+    private VehicleType vehicleType;
+
+    private ParkingStatus parkingStatus;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "parkingSpot")
+    private List<Booking> bookings;
+
+
+
+
+
+
+
+    public UUID getParkingId() {
+        return parkingId;
+    }
+
+    public void setParkingId(UUID parkingId) {
+        this.parkingId = parkingId;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getPricePerHour() {
+        return pricePerHour;
+    }
+
+    public void setPricePerHour(double pricePerHour) {
+        this.pricePerHour = pricePerHour;
+    }
+
+    public VehicleType getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(VehicleType vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public ParkingStatus getParkingStatus() {
+        return parkingStatus;
+    }
+
+    public void setParkingStatus(ParkingStatus parkingStatus) {
+        this.parkingStatus = parkingStatus;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+}
