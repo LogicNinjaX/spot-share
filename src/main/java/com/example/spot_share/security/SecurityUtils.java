f8f78f05afcc316ac8c20af4cdbbeb99574743cb
Nitish <mail.logicninjax@gmail.com>
2025-05-02 22:00:07 +0530

    add utility class for spring security

---
+++ src/main/java/com/example/spot_share/security/SecurityUtils.java
@@ -0,0 +1,17 @@
+package com.example.spot_share.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.UUID;
+
+public class SecurityUtils {
+
+    public static UUID getCurrentUserId() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.getPrincipal() instanceof CustomUserDetails) {
+            return ((CustomUserDetails) auth.getPrincipal()).getUserId();
+        }
+        throw new RuntimeException("User not authenticated");
+    }
+}
