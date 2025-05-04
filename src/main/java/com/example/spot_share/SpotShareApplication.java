82d65971ddb1aad1bd9dcf5ec92859f4d60dd3e1
Nitish <mail.logicninjax@gmail.com>
2025-05-04 20:14:34 +0530

    add redis annotations to enable caching

--- src/main/java/com/example/spot_share/SpotShareApplication.java
+++ src/main/java/com/example/spot_share/SpotShareApplication.java
@@ -2,8 +2,10 @@ package com.example.spot_share;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SpotShareApplication {
 
 	public static void main(String[] args) {
