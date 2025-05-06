d8f7f8069351bfe4cb913202597058ab044adaf7
Nitish <mail.logicninjax@gmail.com>
2025-05-06 11:04:32 +0530

    add new method to fetch parking spot

--- src/main/java/com/example/spot_share/repository/ParkingSpotRepository.java
+++ src/main/java/com/example/spot_share/repository/ParkingSpotRepository.java
@@ -4,10 +4,12 @@ import com.example.spot_share.entity.ParkingSpot;
 import com.example.spot_share.util.dto.ParkingSpotDtoWithoutBookings;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, UUID> {
@@ -15,5 +17,9 @@ public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, UUID> 
     @Query("SELECT new com.example.spot_share.util.dto.ParkingSpotDtoWithoutBookings(p.parkingId, p.location, p.pricePerHour, p.vehicleType, p.parkingStatus, p.createdAt, p.updatedAt)" +
             "FROM ParkingSpot p WHERE p.owner.userId = :userId")
     Page<ParkingSpotDtoWithoutBookings> getParkingList(@Param("userId") UUID userId, Pageable pageable);
+
+    @EntityGraph(attributePaths = {})
+    @Query("SELECT p FROM ParkingSpot p WHERE p.parkingId = :parkingId")
+    Optional<ParkingSpot> getParkingSpot(@Param("parkingId") UUID parkingId);  // // No associations fetched
 
 }
