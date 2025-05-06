12441d2183c2bdd50cf254c1bc0c0d048d1f1070
Nitish <mail.logicninjax@gmail.com>
2025-05-06 11:05:56 +0530

    add new method for parking spot and implement interface method

--- src/main/java/com/example/spot_share/service/ParkingSpotService.java
+++ src/main/java/com/example/spot_share/service/ParkingSpotService.java
@@ -1,10 +1,12 @@
 package com.example.spot_share.service;
 
 import com.example.spot_share.entity.ParkingSpot;
+import com.example.spot_share.util.api_response.UpdateParkingSpotRequest;
 import com.example.spot_share.util.dto.ParkingSpotDtoWithoutBookings;
 import com.example.spot_share.util.dto.ParkingSpotDto;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface ParkingSpotService {
 
@@ -12,4 +14,6 @@ public interface ParkingSpotService {
 
 
     List<ParkingSpotDtoWithoutBookings> getParkingList(int pageNumber, int pageSize);
+
+    ParkingSpotDto updateParkingSpot(UUID parkingId, UpdateParkingSpotRequest request);
 }
--- src/main/java/com/example/spot_share/service/impl/ParkingSpotServiceImpl.java
+++ src/main/java/com/example/spot_share/service/impl/ParkingSpotServiceImpl.java
@@ -5,13 +5,12 @@ import com.example.spot_share.entity.User;
 import com.example.spot_share.repository.ParkingSpotRepository;
 import com.example.spot_share.security.SecurityUtils;
 import com.example.spot_share.service.ParkingSpotService;
+import com.example.spot_share.util.api_response.UpdateParkingSpotRequest;
 import com.example.spot_share.util.dto.ParkingSpotDto;
 import com.example.spot_share.util.dto.ParkingSpotDtoWithoutBookings;
 import jakarta.persistence.EntityManager;
 import org.modelmapper.ModelMapper;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -46,6 +45,31 @@ public class ParkingSpotServiceImpl implements ParkingSpotService {
     public List<ParkingSpotDtoWithoutBookings> getParkingList(int pageNumber, int pageSize) {
         UUID currentUserId = SecurityUtils.getCurrentUserId();
         return parkingSpotRepository.getParkingList(currentUserId, PageRequest.of(pageNumber-1, pageSize)).stream().toList();
+    }
+
+    @Override
+    public ParkingSpotDto updateParkingSpot(UUID parkingId, UpdateParkingSpotRequest request) { // for complete
+
+        ParkingSpot currentParkingSpot = parkingSpotRepository.getParkingSpot(parkingId).map(parkingSpot -> {
+            parkingSpot.setLocation(request.getLocation());
+            parkingSpot.setPricePerHour(request.getPricePerHour());
+            parkingSpot.setVehicleType(request.getVehicleType());
+            parkingSpot.setParkingStatus(request.getParkingStatus());
+            return parkingSpot;
+        }).orElseThrow(() -> new RuntimeException("parking spot not found with id:"+parkingId));
+
+        ParkingSpot updatedParkingSpot = parkingSpotRepository.save(currentParkingSpot); // after mapping
+
+        ParkingSpotDto parkingSpotDto = new ParkingSpotDto();
+        parkingSpotDto.setParkingId(updatedParkingSpot.getParkingId());
+        parkingSpotDto.setLocation(updatedParkingSpot.getLocation());
+        parkingSpotDto.setPricePerHour(updatedParkingSpot.getPricePerHour());
+        parkingSpotDto.setVehicleType(updatedParkingSpot.getVehicleType());
+        parkingSpotDto.setParkingStatus(updatedParkingSpot.getParkingStatus());
+        parkingSpotDto.setCreatedAt(updatedParkingSpot.getCreatedAt());
+        parkingSpotDto.setUpdatedAt(updatedParkingSpot.getUpdatedAt());
+
+        return parkingSpotDto;
     }
 
 
