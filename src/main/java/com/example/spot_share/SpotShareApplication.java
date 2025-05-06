8e17a39d95437c16ce1353ade9f824a65be9a270
Nitish <mail.logicninjax@gmail.com>
2025-05-06 11:02:00 +0530

    add @EnableMethodSecurity annotation

--- src/main/java/com/example/spot_share/SpotShareApplication.java
+++ src/main/java/com/example/spot_share/SpotShareApplication.java
@@ -3,9 +3,11 @@ package com.example.spot_share;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @EnableCaching
+@EnableMethodSecurity
 public class SpotShareApplication {
 
 	public static void main(String[] args) {
