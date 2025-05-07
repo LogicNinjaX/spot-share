df959c0323cc4657d4cef378ff3c94b471aa2880
Nitish <mail.logicninjax@gmail.com>
2025-05-07 13:53:18 +0530

    update api response in parking controller

--- src/main/java/com/example/spot_share/controller/ParkingController.java
+++ src/main/java/com/example/spot_share/controller/ParkingController.java
@@ -1,12 +1,17 @@
 package com.example.spot_share.controller;
 
+import com.example.spot_share.entity.ParkingSpot;
 import com.example.spot_share.enums.ParkingStatus;
 import com.example.spot_share.service.ParkingSpotService;
+import com.example.spot_share.util.api_response.ApiResponse;
+import com.example.spot_share.util.api_response.SavedParkingResponse;
 import com.example.spot_share.util.api_response.UpdateParkingSpotRequest;
 import com.example.spot_share.util.api_response.UpdatedParkingSpotResponse;
 import com.example.spot_share.util.dto.ParkingSpotDto;
+import com.example.spot_share.util.dto.ParkingSpotDtoWithoutBookings;
 import com.example.spot_share.util.dto.RegisterParkingSpot;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -27,23 +32,28 @@ public class ParkingController {
     }
 
     @PostMapping("/parking-spots")
-    public String saveParkingSpot(@RequestBody RegisterParkingSpot registerParkingSpot){
+    public ResponseEntity<ApiResponse<SavedParkingResponse>> saveParkingSpot(@RequestBody RegisterParkingSpot registerParkingSpot){
         ParkingSpotDto parkingSpotDto = modelMapper.map(registerParkingSpot, ParkingSpotDto.class);
         parkingSpotDto.setParkingStatus(ParkingStatus.AVAILABLE);
-        parkingSpotService.saveParkingSpot(parkingSpotDto);
-        return "saved";
+        ParkingSpot parkingSpot = parkingSpotService.saveParkingSpot(parkingSpotDto);
+        SavedParkingResponse response = modelMapper.map(parkingSpot, SavedParkingResponse.class);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new ApiResponse<>(true, "parking space has been created successfully", response));
     }
 
     @PreAuthorize("hasRole('OWNER')")
     @GetMapping("/parking-spots/my")
-    public List<?> getParkingList(@RequestParam int pageNumber, @RequestParam int pageSize){
-        return parkingSpotService.getParkingList(pageNumber, pageSize);
+    public ResponseEntity<ApiResponse<List<ParkingSpotDtoWithoutBookings>>> getParkingList(@RequestParam int pageNumber, @RequestParam int pageSize){
+        List<ParkingSpotDtoWithoutBookings> parkingList =  parkingSpotService.getParkingList(pageNumber, pageSize);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(true,"data retrieved successfully", parkingList));
     }
 
     @PreAuthorize("hasRole('OWNER')")
     @PutMapping("/parking-spots/{parking-id}")
-    public ResponseEntity<?> updateParkingSpot(@RequestBody UpdateParkingSpotRequest updateRequest, @PathVariable("parking-id") UUID parkingId){
+    public ResponseEntity<ApiResponse<?>> updateParkingSpot(@RequestBody UpdateParkingSpotRequest updateRequest, @PathVariable("parking-id") UUID parkingId){
         ParkingSpotDto parkingSpotDto = parkingSpotService.updateParkingSpot(parkingId,updateRequest);
-        return ResponseEntity.ok(modelMapper.map(parkingSpotDto, UpdatedParkingSpotResponse.class));
+        UpdatedParkingSpotResponse updateResponse = modelMapper.map(parkingSpotDto, UpdatedParkingSpotResponse.class);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<>(true, "parking details updated successfully", updateResponse));
     }
 }
