75e7860d0226c4a99467813cb8510d4d9325bfec
Nitish <mail.logicninjax@gmail.com>
2025-05-21 08:07:54 +0530

    add custom exception for parking

---
+++ src/main/java/com/example/spot_share/util/exception/ParkingException.java
@@ -0,0 +1,8 @@
+package com.example.spot_share.util.exception;
+
+public class ParkingException extends RuntimeException{
+
+    public ParkingException(String message) {
+        super(message);
+    }
+}
