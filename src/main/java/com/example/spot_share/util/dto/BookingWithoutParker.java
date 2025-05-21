842e4f9d0d96197e0facae7642684ae98545be54
Nitish <mail.logicninjax@gmail.com>
2025-05-21 08:48:29 +0530

    add booking dto

---
+++ src/main/java/com/example/spot_share/util/dto/BookingWithoutParker.java
@@ -0,0 +1,16 @@
+package com.example.spot_share.util.dto;
+
+import com.example.spot_share.entity.ParkingSpot;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record BookingWithoutParker(
+        UUID bookingId,
+        LocalDateTime bookedAt,
+        boolean isActive,
+        String parkingLocation,
+        double pricePerHour,
+        String parkingOwner
+)
+{ }
