f4a6b6e74c343dd388d7a7b549684a7140d1c4a7
Nitish <mail.logicninjax@gmail.com>
2025-05-21 10:28:02 +0530

    add dto class

---
+++ src/main/java/com/example/spot_share/util/dto/ParkingDetails.java
@@ -0,0 +1,18 @@
+package com.example.spot_share.util.dto;
+
+import com.example.spot_share.enums.ParkingStatus;
+import com.example.spot_share.enums.VehicleType;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record ParkingDetails(
+        UUID parkingId,
+        String location,
+        double pricePerHour,
+        VehicleType vehicleType,
+        ParkingStatus parkingStatus,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
+}
