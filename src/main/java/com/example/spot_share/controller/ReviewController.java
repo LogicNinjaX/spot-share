9aa04093958d1c79cec173c4406154b2b3c8ebc0
Nitish <mail.logicninjax@gmail.com>
2025-05-24 23:07:09 +0530

    add rest controller for review submission

---
+++ src/main/java/com/example/spot_share/controller/ReviewController.java
@@ -0,0 +1,37 @@
+package com.example.spot_share.controller;
+
+import com.example.spot_share.entity.Review;
+import com.example.spot_share.service.ReviewService;
+import com.example.spot_share.util.api_response.ApiResponse;
+import com.example.spot_share.util.api_response.ReviewRequest;
+import com.example.spot_share.util.api_response.ReviewResponse;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ReviewController {
+
+    private final ReviewService reviewService;
+    private final ModelMapper modelMapper;
+
+    public ReviewController(ReviewService reviewService, ModelMapper modelMapper) {
+        this.reviewService = reviewService;
+        this.modelMapper = modelMapper;
+    }
+
+    @PostMapping("/reviews")
+    public ResponseEntity<ApiResponse<ReviewResponse>> submitReview(@RequestBody ReviewRequest reviewRequest){
+        Review review = reviewService.saveReview(reviewRequest);
+
+        ReviewResponse reviewResponse = modelMapper.map(review, ReviewResponse.class);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<>(true, "request submitted", reviewResponse));
+    }
+}
