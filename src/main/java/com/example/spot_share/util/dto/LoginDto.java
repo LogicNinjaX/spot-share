b7fa54409766eb52ab1a011e2a496d860d37420f
Nitish <mail.logicninjax@gmail.com>
2025-05-29 16:03:27 +0530

    add bean validation annotion

--- src/main/java/com/example/spot_share/util/dto/LoginDto.java
+++ src/main/java/com/example/spot_share/util/dto/LoginDto.java
@@ -1,9 +1,13 @@
 package com.example.spot_share.util.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public class LoginDto {
 
+    @NotEmpty(message = "Username is required")
     private String username;
 
+    @NotEmpty(message = "Password is required")
     private String password;
 
     public String getUsername() {
--- src/main/java/com/example/spot_share/util/dto/RegisterDto.java
+++ src/main/java/com/example/spot_share/util/dto/RegisterDto.java
@@ -1,9 +1,16 @@
 package com.example.spot_share.util.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class RegisterDto {
 
+    @NotEmpty(message = "Username is required")
+    @Size(min = 5, max = 15, message = "username must be 5 characters long")
     private String username;
 
+    @NotEmpty(message = "Password is required")
+    @Size(min = 8, max = 20, message = "password must be 8 characters long")
     private String password;
 
 
