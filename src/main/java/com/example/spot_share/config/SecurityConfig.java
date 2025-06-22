9b23bf3832fab061e56a26b537bcf6c641efaed9
Nitish <mail.logicninjax@gmail.com>
2025-06-22 20:18:00 +0530

    update class

--- src/main/java/com/example/spot_share/config/SecurityConfig.java
+++ src/main/java/com/example/spot_share/config/SecurityConfig.java
@@ -26,8 +26,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/parkers/register", "/api/v1/parkers/login").permitAll()
-                        .requestMatchers("/api/v1/owners/register", "/api/v1/owners/login").permitAll()
+                        .requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
