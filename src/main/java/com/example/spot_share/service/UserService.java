b69f440763806fbc42d7bf75c19ba9f9405212fc
Nitish <mail.logicninjax@gmail.com>
2025-06-23 19:19:22 +0530

    add service method

--- src/main/java/com/example/spot_share/service/UserService.java
+++ src/main/java/com/example/spot_share/service/UserService.java
@@ -2,8 +2,13 @@ package com.example.spot_share.service;
 
 import com.example.spot_share.entity.User;
 import com.example.spot_share.util.dto.UserDto;
+import com.example.spot_share.util.exception.UserNotFoundException;
+
+import java.util.UUID;
 
 public interface UserService {
 
     User saveUser(UserDto userDto);
+
+    User updateUser(UUID userId, User user) throws UserNotFoundException;
 }
