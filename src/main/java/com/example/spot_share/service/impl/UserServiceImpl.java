05eba3dca62d7eb0a0964a8d32042e933e16e709
Nitish <mail.logicninjax@gmail.com>
2025-06-23 19:19:48 +0530

    add service method implementation

--- src/main/java/com/example/spot_share/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/spot_share/service/impl/UserServiceImpl.java
@@ -4,9 +4,12 @@ import com.example.spot_share.entity.User;
 import com.example.spot_share.repository.UserRepository;
 import com.example.spot_share.service.UserService;
 import com.example.spot_share.util.dto.UserDto;
+import com.example.spot_share.util.exception.UserNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -26,5 +29,15 @@ public class UserServiceImpl implements UserService {
         userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
         User user = modelMapper.map(userDto, User.class);
         return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(UUID userId, User user){
+        User userEntity = userRepository.findById(userId).orElseThrow(()-> new UserNotFoundException("User not found"));
+
+        userEntity.setUsername(user.getUsername());
+        userEntity.setPassword(user.getPassword());
+        userEntity.setEmail(user.getEmail());
+        return userRepository.save(userEntity);
     }
 }
